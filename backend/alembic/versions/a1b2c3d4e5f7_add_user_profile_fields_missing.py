568579876cef2db5a02a10d67c3ebc52598d3cd4
Chai-B <bansalchaitanya1234@gmail.com>
2026-02-25 21:00:00 -0600

    fix: resolve alembic multiple heads by merging branches

--- backend/alembic/versions/a1b2c3d4e5f7_add_user_profile_fields_missing.py
+++ backend/alembic/versions/a1b2c3d4e5f7_add_user_profile_fields_missing.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f7'
-down_revision: Union[str, Sequence[str], None] = 'f8g9h0i1j2k3'
+down_revision: Union[str, Sequence[str], None] = 'ccd3d3906fd4'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
